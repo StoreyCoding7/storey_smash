1fb1be420131345800c00fde76125bf24f507330
STOREY.501 <cameronstorey4@gmail.com>
2024-12-22 18:35:38 +0000

    created and synced postgres database to node

---
+++ Database/script.js
@@ -0,0 +1,31 @@
+const {Client} = require('pg');
+
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "storey_smash",
+  password: "postgres",
+  port: 5432
+})
+
+client.connect();
+
+
+//USERS
+client.query(`Select * from users`, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+})
+//PRODUCTS
+client.query(`Select * from products`, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+})
